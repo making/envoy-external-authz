bdbe8149847e2451325d69f40ecccf8bf9cb0112
Toshiaki Maki <makingx@gmail.com>
2025-07-22 14:51:35 +0900

    Update

--- src/main/java/am/ik/envoy/AuthorizationServiceImpl.java
+++ src/main/java/am/ik/envoy/AuthorizationServiceImpl.java
@@ -13,6 +13,7 @@ import io.envoyproxy.envoy.service.auth.v3.OkHttpResponse;
 import io.envoyproxy.envoy.type.v3.HttpStatus;
 import io.envoyproxy.envoy.type.v3.StatusCode;
 import io.grpc.stub.StreamObserver;
+import java.util.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -24,18 +25,42 @@ public class AuthorizationServiceImpl extends AuthorizationGrpc.AuthorizationImp
 
 	@Override
 	public void check(CheckRequest request, StreamObserver<CheckResponse> responseObserver) {
+		if (logger.isTraceEnabled()) {
+			logger.trace("request details: {}", request);
+		}
 		AttributeContext.HttpRequest req = request.getAttributes().getRequest().getHttp();
 		if (logger.isInfoEnabled()) {
 			logger.info("Check request host={} path={} id={}", req.getHost(), req.getPath(), req.getId());
 		}
-		System.out.println(request);
-		// req.getHeaderMap().getExtension()
-		String user = req.getHeadersMap().get("x-user");
-		if ("demo".equalsIgnoreCase(user)) {
+		String authorization = req.getHeadersMap().get("authorization");
+		if (authorization == null || !authorization.startsWith("Basic ")) {
+			responseObserver.onNext(CheckResponse.newBuilder()
+				.setStatus(Status.newBuilder().setCode(Code.UNAUTHENTICATED_VALUE).build())
+				.setDeniedResponse(DeniedHttpResponse.newBuilder()
+					.setStatus(HttpStatus.newBuilder().setCode(StatusCode.Unauthorized).build())
+					.addHeaders(HeaderValueOption.newBuilder()
+						.setHeader(HeaderValue.newBuilder()
+							.setKey("X-Auth-Handler")
+							.setValue("am.ik.envoy.AuthorizationServiceImpl.check")
+							.build())
+						.build())
+					.build())
+				.build());
+			responseObserver.onCompleted();
+			return;
+		}
+
+		String basic = new String(Base64.getDecoder().decode(authorization.substring("Basic ".length())));
+		String[] parts = basic.split(":", 2);
+		String user = parts[0];
+		String password = parts.length > 1 ? parts[1] : "";
+		logger.info("Authenticating user={}", user);
+		if ("demo".equals(user) && "password".equals(password)) {
 			responseObserver.onNext(CheckResponse.newBuilder()
 				.setStatus(Status.newBuilder().setCode(Code.OK_VALUE).build())
 				.setOkResponse(OkHttpResponse.newBuilder()
 					.addHeaders(HeaderValueOption.newBuilder()
+						.setHeader(HeaderValue.newBuilder().setKey("X-User").setValue(user).build())
 						.setHeader(HeaderValue.newBuilder()
 							.setKey("X-Auth-Handler")
 							.setValue("am.ik.envoy.AuthorizationServiceImpl.check")
@@ -48,7 +73,7 @@ public class AuthorizationServiceImpl extends AuthorizationGrpc.AuthorizationImp
 			responseObserver.onNext(CheckResponse.newBuilder()
 				.setStatus(Status.newBuilder().setCode(Code.PERMISSION_DENIED_VALUE).build())
 				.setDeniedResponse(DeniedHttpResponse.newBuilder()
-					.setStatus(HttpStatus.newBuilder().setCode(StatusCode.Unauthorized).build())
+					.setStatus(HttpStatus.newBuilder().setCode(StatusCode.Forbidden).build())
 					.addHeaders(HeaderValueOption.newBuilder()
 						.setHeader(HeaderValue.newBuilder()
 							.setKey("X-Auth-Handler")
--- src/test/java/am/ik/envoy/AuthorizationServiceImplTest.java
+++ src/test/java/am/ik/envoy/AuthorizationServiceImplTest.java
@@ -1,9 +1,15 @@
 package am.ik.envoy;
 
+import com.google.protobuf.Timestamp;
+import com.google.rpc.Code;
+import io.envoyproxy.envoy.config.core.v3.Address;
+import io.envoyproxy.envoy.config.core.v3.Metadata;
+import io.envoyproxy.envoy.config.core.v3.SocketAddress;
 import io.envoyproxy.envoy.service.auth.v3.AttributeContext;
 import io.envoyproxy.envoy.service.auth.v3.AuthorizationGrpc;
 import io.envoyproxy.envoy.service.auth.v3.CheckRequest;
 import io.envoyproxy.envoy.service.auth.v3.CheckResponse;
+import io.envoyproxy.envoy.type.v3.StatusCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,36 +29,119 @@ class AuthorizationServiceImplTest {
 	void testOk() throws Exception {
 		CheckResponse checkResponse = this.stub.check(CheckRequest.newBuilder()
 			.setAttributes(AttributeContext.newBuilder()
+				.setSource(AttributeContext.Peer.newBuilder()
+					.setAddress(Address.newBuilder()
+						.setSocketAddress(SocketAddress.newBuilder().setAddress("192.168.11.12").setPortValue(59206))))
+				.setDestination(AttributeContext.Peer.newBuilder()
+					.setAddress(Address.newBuilder()
+						.setSocketAddress(SocketAddress.newBuilder().setAddress("10.1.42.159").setPortValue(8443)))
+					.setPrincipal("*.example.com"))
 				.setRequest(AttributeContext.Request.newBuilder()
+					.setTime(Timestamp.newBuilder().setSeconds(1753161991).setNanos(443256000))
 					.setHttp(AttributeContext.HttpRequest.newBuilder()
-						.setHost("example.com")
+						.setId("10878827033623646048")
+						.setMethod("GET")
+						.putHeaders("authorization", "Basic ZGVtbzpwYXNzd29yZA==")
 						.setPath("/")
-						.setId("12345")
-						.putHeaders("x-user", "demo")
-						.build())
-					.build())
-				.build())
+						.setHost("example.com")
+						.setScheme("https")
+						.setProtocol("HTTP/2")))
+				.setMetadataContext(Metadata.newBuilder())
+				.setRouteMetadataContext(Metadata.newBuilder()))
 			.build());
 		assertThat(checkResponse.hasOkResponse()).isTrue();
 		assertThat(checkResponse.hasDeniedResponse()).isFalse();
+		assertThat(checkResponse.getStatus().getCode()).isEqualTo(Code.OK_VALUE);
 	}
 
 	@Test
-	void testDenied() throws Exception {
+	void testDeniedWrongPassword() throws Exception {
 		CheckResponse checkResponse = this.stub.check(CheckRequest.newBuilder()
 			.setAttributes(AttributeContext.newBuilder()
+				.setSource(AttributeContext.Peer.newBuilder()
+					.setAddress(Address.newBuilder()
+						.setSocketAddress(SocketAddress.newBuilder().setAddress("192.168.11.12").setPortValue(59206))))
+				.setDestination(AttributeContext.Peer.newBuilder()
+					.setAddress(Address.newBuilder()
+						.setSocketAddress(SocketAddress.newBuilder().setAddress("10.1.42.159").setPortValue(8443)))
+					.setPrincipal("*.example.com"))
 				.setRequest(AttributeContext.Request.newBuilder()
+					.setTime(Timestamp.newBuilder().setSeconds(1753161991).setNanos(443256000))
 					.setHttp(AttributeContext.HttpRequest.newBuilder()
-						.setHost("example.com")
+						.setId("10878827033623646048")
+						.setMethod("GET")
+						.putHeaders("authorization", "Basic ZGVtbzpwYXNzd29yZDI=")
 						.setPath("/")
-						.setId("12345")
-						.putHeaders("x-user", "bar")
-						.build())
-					.build())
-				.build())
+						.setHost("example.com")
+						.setScheme("https")
+						.setProtocol("HTTP/2")))
+				.setMetadataContext(Metadata.newBuilder())
+				.setRouteMetadataContext(Metadata.newBuilder()))
 			.build());
 		assertThat(checkResponse.hasOkResponse()).isFalse();
 		assertThat(checkResponse.hasDeniedResponse()).isTrue();
+		assertThat(checkResponse.getDeniedResponse().getStatus().getCode()).isEqualTo(StatusCode.Forbidden);
+		assertThat(checkResponse.getStatus().getCode()).isEqualTo(Code.PERMISSION_DENIED_VALUE);
+	}
+
+	@Test
+	void testDeniedNotExistingUser() throws Exception {
+		CheckResponse checkResponse = this.stub.check(CheckRequest.newBuilder()
+			.setAttributes(AttributeContext.newBuilder()
+				.setSource(AttributeContext.Peer.newBuilder()
+					.setAddress(Address.newBuilder()
+						.setSocketAddress(SocketAddress.newBuilder().setAddress("192.168.11.12").setPortValue(59206))))
+				.setDestination(AttributeContext.Peer.newBuilder()
+					.setAddress(Address.newBuilder()
+						.setSocketAddress(SocketAddress.newBuilder().setAddress("10.1.42.159").setPortValue(8443)))
+					.setPrincipal("*.example.com"))
+				.setRequest(AttributeContext.Request.newBuilder()
+					.setTime(Timestamp.newBuilder().setSeconds(1753161991).setNanos(443256000))
+					.setHttp(AttributeContext.HttpRequest.newBuilder()
+						.setId("10878827033623646048")
+						.setMethod("GET")
+						.putHeaders("authorization", "Basic YmFyOnBhc3N3b3Jk")
+						.setPath("/")
+						.setHost("example.com")
+						.setScheme("https")
+						.setProtocol("HTTP/2")))
+				.setMetadataContext(Metadata.newBuilder())
+				.setRouteMetadataContext(Metadata.newBuilder()))
+			.build());
+		assertThat(checkResponse.hasOkResponse()).isFalse();
+		assertThat(checkResponse.hasDeniedResponse()).isTrue();
+		assertThat(checkResponse.getDeniedResponse().getStatus().getCode()).isEqualTo(StatusCode.Forbidden);
+		assertThat(checkResponse.getStatus().getCode()).isEqualTo(Code.PERMISSION_DENIED_VALUE);
+	}
+
+	@Test
+	void testDeniedNoAuthorization() throws Exception {
+		CheckResponse checkResponse = this.stub.check(CheckRequest.newBuilder()
+			.setAttributes(AttributeContext.newBuilder()
+				.setSource(AttributeContext.Peer.newBuilder()
+					.setAddress(Address.newBuilder()
+						.setSocketAddress(SocketAddress.newBuilder().setAddress("192.168.11.12").setPortValue(59206))))
+				.setDestination(AttributeContext.Peer.newBuilder()
+					.setAddress(Address.newBuilder()
+						.setSocketAddress(SocketAddress.newBuilder().setAddress("10.1.42.159").setPortValue(8443)))
+					.setPrincipal("*.example.com"))
+				.setRequest(AttributeContext.Request.newBuilder()
+					.setTime(Timestamp.newBuilder().setSeconds(1753161991).setNanos(443256000))
+					.setHttp(AttributeContext.HttpRequest.newBuilder()
+						.setId("10878827033623646048")
+						.setMethod("GET")
+						.putHeaders("foo", "bar")
+						.setPath("/")
+						.setHost("example.com")
+						.setScheme("https")
+						.setProtocol("HTTP/2")))
+				.setMetadataContext(Metadata.newBuilder())
+				.setRouteMetadataContext(Metadata.newBuilder()))
+			.build());
+		assertThat(checkResponse.hasOkResponse()).isFalse();
+		assertThat(checkResponse.hasDeniedResponse()).isTrue();
+		assertThat(checkResponse.getDeniedResponse().getStatus().getCode()).isEqualTo(StatusCode.Unauthorized);
+		assertThat(checkResponse.getStatus().getCode()).isEqualTo(Code.UNAUTHENTICATED_VALUE);
 	}
 
 }
